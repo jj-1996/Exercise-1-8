f5b386337c70c0ce7e4b21258856828042e27f30
jj-1996 <39858525+jj-1996@users.noreply.github.com>
2018-06-02 14:29:27 +0100

    Solution commit

---
+++ Exercise 1-8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exercise_1_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Current Date & Time: " + DateTime.Now.ToString());
+            Console.ReadKey();
+        }
+    }
+}
